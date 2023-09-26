987c1594664f290bc8425a3ef139efffca5cf043
Gabriel Okura <gabriellevicz@gmail.com>
2023-09-26 18:29:33 -0300

    Initial Commit

---
+++ MyCollection/MyCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionApp.swift
+//  MyCollection
+//
+//  Created by Gabriel Motelevicz Okura on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
